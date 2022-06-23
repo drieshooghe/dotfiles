48369212b3e9d8e7949ee51b2fa95994dbb52851
Dries Hooghe <dries@drieshooghe.com>
2022-06-23 17:53:23 +0200

    :tada: create a dotfiles project

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          echo "${unameOut} is not a supported dotfile environment" && exit 1;;
+esac
+
+echo $machine
